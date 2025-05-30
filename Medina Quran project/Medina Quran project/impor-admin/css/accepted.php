55187df62b65deae0447bdfe0323a29946bddd66
Abdulrahman Alameri <65046997+4ini@users.noreply.github.com>
2025-05-30 11:23:31 +0400

    Add files via upload

---
+++ Medina Quran project/Medina Quran project/impor-admin/css/accepted.php
@@ -0,0 +1,52 @@
+
+
+
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+<center>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Draste Students registeration</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/JannaLTRegular.css"></head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<body>
+    <div class="main">
+        <div class="logo">
+            <img src="images/bg5.png">
+            <h2>مركز المدينة لتحفيظ القرآن الكريم</h2>
+        </div>
+        <div class="book">
+            <p style="color:red">اختر قسم المقبولين : </p>
+            <div class="container ">
+                          <div class="text-center ">
+                          <a style="background-color: cadetblue; margin-top: 10px;"href="../admin2/accepted.php" name="bt" type="button" class="btn btn-lg bt bt3 btn-secondary">طالبات المدراس</a>
+                           <br>
+                           <a style="background-color: cadetblue; margin-top: 10px;" href="../admin1/accepted.php" name="bt" type="button" class="btn  btn-lg bt bt3 btn-secondary">الكبار +18</a>
+                            <br>
+                            <a style="background-color:cadetblue; margin-top: 10px;"href="../admin3/accepted.php" name="bt" type="button" class="btn btn-lg bt bt3 btn-secondary">بركة الدار</a>
+                            <br>
+                            <a style="background-color:cadetblue; margin-top: 10px;"href="../admin4/accepted.php" name="bt" type="button" class="btn btn-lg bt bt3 btn-secondary">الأعاجم</a>
+                            <br>
+                            <a style="background-color:cadetblue; margin-top: 10px;"href="../admin5/accepted.php" name="bt" type="button" class="btn btn-lg bt bt3 btn-secondary">أصحاب الهمم</a>
+                            
+                            
+            
+
+<br><br>
+</div>
+</center>
+</body>
+</html>
+
---
+++ Medina Quran project/Medina Quran project/impor-admin/login.php
@@ -0,0 +1,53 @@
+
+
+
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+<center>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Draste Students registeration</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/JannaLTRegular.css"></head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<body>
+    <div class="main">
+        <div class="logo">
+            <img src="images/bg5.png">
+            <h2>مركز المدينة لتحفيظ القرآن الكريم</h2>
+        </div>
+        <div class="book">
+            <p style="color:red">اختر القسم : </p>
+            <div class="container ">
+                          <div class="text-center ">
+                          <a style="background-color: cadetblue; margin-top: 10px;"href="../admin2/admin.php" name="bt" type="button" class="btn btn-lg bt bt3 btn-secondary">طالبات المدارس</a>
+                           <br>
+                           <a style="background-color: cadetblue; margin-top: 10px;" href="../admin1/admin.php" name="bt" type="button" class="btn  btn-lg bt bt3 btn-secondary">الكبار +18</a>
+                            <br>
+                            <a style="background-color:cadetblue; margin-top: 10px;"href="../admin3/admin.php" name="bt" type="button" class="btn btn-lg bt bt3 btn-secondary">بركة الدار</a>
+                            <br>
+                            <a style="background-color:cadetblue; margin-top: 10px;"href="../admin4/admin.php" name="bt" type="button" class="btn btn-lg bt bt3 btn-secondary">الأعاجم</a>
+                            <br>
+                            <a style="background-color:cadetblue; margin-top: 10px;"href="../admin5/admin.php" name="bt" type="button" class="btn btn-lg bt bt3 btn-secondary">أصحاب الهمم</a>
+                            <br>
+                            <a style="background-color:#A52A2A; margin-top: 10px;"href="accepted.php" name="bt" type="button" class="btn btn-lg bt bt3 btn-secondary">عرض المقبولين</a>
+                            
+            
+
+<br><br>
+</div>
+</center>
+</body>
+</html>
+
